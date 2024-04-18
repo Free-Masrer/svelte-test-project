905c1c56e87d0b7c8e1a7cba7d8dd76a70129445
Denys <samadhiprojekt@gmail.com>
2024-04-18 12:46:56 +0300

    Add icon

--- src/lib/TextInput/TextInput.svelte
+++ src/lib/TextInput/TextInput.svelte
@@ -12,11 +12,32 @@
   </script>
   
   <form on:submit|preventDefault={handleSubmit}>
-    <input type="text" bind:value={text} placeholder="Type your message here...">
+    <div class="chat-field">
+    <input type="text" bind:value={text} placeholder="Ask Assistant" class="text-input">
     <button type="submit" disabled={text.trim() === ''}>Send</button>
+</div>
   </form>
   
   <style>
-    /* styles here */
+    .chat-field {
+        display: flex;
+        padding: var(--Spacing-space-none, 0px) var(--Spacing-space-xs, 15px);
+        flex-direction: column;
+        justify-content: flex-end;
+        align-items: flex-start;
+        gap: var(--Spacing-space-xxs, 10px);
+        align-self: stretch;
+    }
+
+    .text-input {
+        display: flex;
+        padding: 12px 16px;
+        align-items: flex-start;
+        align-self: stretch;
+        border-radius: 10px;
+        border: 1px solid var(--gradient-gradient-indigo, #473FFF);
+
+        background: var(--white, #FFF);
+    }
   </style>
   
